e5cbe428474c1c878c7905c8cccce7f0afe95baa
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-01-31 23:33:56 -0300

    cortes

--- notas.clj
+++ notas.clj
@@ -2,19 +2,10 @@ notas:
 
 
 HTML
-(x)Fazer o mapa no projeto no html.
-(x)criar cada conteiner sequindo as regras.
-(x)criar os botões e os links.
-()criar botoes 
-()deixar os botoes funcionais
+
 
 
 CSS
-(x)editar tudo no css, ajustando o tamanho.
-(x)escolher as melhores cores simetricas.
-()fazer o footer
-()selecionar as melhores fotos e propagandas.
-()ajustar a melhor fonte
+()diminuir o tamanho das imagens
 
 JavaScript
-()vincular no js, junto com o projeto ja existente.
